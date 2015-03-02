369c8edcb9441c734c2bc8eccdf26654cf22b09d
victorkwak <victorkwak@gmail.com>
2015-03-01 16:45:10 -0800

    Initial Commit

---
+++ src/HostsGrabber.java
@@ -0,0 +1,9 @@
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
+public class HostsGrabber {
+
+    public void closeProgram(ActionEvent actionEvent) {
+        System.exit(0);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("../HostsGrabber.fxml"));
+        primaryStage.setTitle("Hosts");
+        primaryStage.setScene(new Scene(root, 380, 400));
+        primaryStage.show();
+    }
+}
