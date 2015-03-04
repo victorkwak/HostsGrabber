b9551e881916805794f87d48770d5d484eb93156
victorkwak <victorkwak@gmail.com>
2015-03-04 01:07:31 -0800

    Started move to JavaFX

--- src/HostsGrabber.java
+++ src/HostsGrabber.java
@@ -1,9 +1,48 @@
 import javafx.event.ActionEvent;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
-public class HostsGrabber {
+import java.net.URL;
+import java.util.ResourceBundle;
 
-    public void closeProgram(ActionEvent actionEvent) {
-        System.exit(0);
+public class HostsGrabber implements Initializable{
+
+    public AnchorPane frame;
+    public PasswordField passwordField;
+    public TextArea processLog;
+    public TextField whitelistURLField;
+    public ListView whitelist;
+
+    public void closeProgram() {
+        Stage stage = (Stage) frame.getScene().getWindow();
+        stage.close();
+    }
+
+    public void showAbout() {
+        Stage about = new Stage();
+        about.setTitle("About");
+    }
+
+    public void getHosts(ActionEvent actionEvent) {
+        System.out.println("Working");
+    }
+
+    public void cancelTask(ActionEvent actionEvent) {
+        System.out.println("Working");
+    }
+
+    public void addToWhitelist(ActionEvent actionEvent) {
+        System.out.println("Working");
+    }
+
+    public void removeFromWhitelist(ActionEvent actionEvent) {
+        System.out.println("Working");
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        whitelistURLField.setText("Initialize working.");
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,11 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../HostsGrabber.fxml"));
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("HostsGrabber.fxml"));
         primaryStage.setTitle("Hosts");
         primaryStage.setScene(new Scene(root, 380, 400));
+        primaryStage.resizableProperty().set(false);
         primaryStage.show();
     }
 }
